e6928f2be26644372f4196557c6b6143e7a53aed
tr2512 <hieu251202@gmail.com>
2022-06-17 23:25:10 +0700

    Add the base code for GUI

---
+++ SourceCode/Main.java
@@ -0,0 +1,29 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Parent;
+import javafx.scene.paint.Color;
+import java.io.IOException;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage stage) {
+	try {	
+		Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("view/MainScreen.fxml"));
+		Scene scene = new Scene(root, Color.BLACK);
+		
+		stage.setTitle("Application");
+		stage.setScene(scene);
+		stage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}	
+}
---
+++ SourceCode/controller/GeneralController.java
@@ -0,0 +1,28 @@
+package controller;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.fxml.FXMLLoader;
+import java.io.IOException;
+import javafx.event.ActionEvent;
+
+public abstract class GeneralController {
+	protected Scene scene;
+	protected Stage stage;
+	protected Parent root;
+	protected FXMLLoader loader;
+	
+	public void switchScene(String xmlDir, ActionEvent e) {
+		try {
+			loader = new FXMLLoader(getClass().getClassLoader().getResource(xmlDir));
+			root = loader.load();
+			stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+			scene = new Scene(root);
+			stage.setScene(scene);
+			stage.show();
+		} catch (IOException except) {
+			except.printStackTrace();
+		}
+	}
+}
