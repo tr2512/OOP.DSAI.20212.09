6510bde475cbccedf1561ab2bd8f1127e2d2e374
tyuukau <bachocnhi2002@gmail.com>
2022-07-15 13:00:00 +0700

    Update Division FXML

---
+++ SourceCode/controller/ProkaryoticDivisionController.java
@@ -0,0 +1,67 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+
+public class ProkaryoticDivisionController {
+
+    @FXML
+    private Label objectField;
+
+    @FXML
+    private Text descriptionField;
+
+    @FXML
+    private ProgressBar progressBar;
+
+    @FXML
+    private Button back;
+
+    @FXML
+    private Button playPauseButton;
+
+    @FXML
+    private ImageView playPauseImageView;
+
+    @FXML
+    private Button next;
+
+    @FXML
+    private Button restart;
+
+    @FXML
+    void backClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void nextClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void restartClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void switchHome(ActionEvent event) {
+
+    }
+
+    @FXML
+    void switchProkaryoticCell(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        
+    }
+
+}
